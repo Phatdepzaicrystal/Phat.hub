77aff7b9ac6da3a6ae56eb7fa77a0e55d32086e6
Phatdepzaicrystal <ngongochungphat55@gmail.com>
2024-11-07 11:36:14 +0700

    Update PhatCrystalzzz.lua

--- PhatCrystalzzz.lua
+++ PhatCrystalzzz.lua
@@ -1,8 +1,8 @@
-print("Break");
+print("ZZZ");
 local v0 = require(game:GetService("ReplicatedStorage").Notification);
-v0.new("<Color=Cyan>Welcome to Xeter Hub<Color=/>"):Display();
+v0.new("<Color=Cyan>Welcome to Phat Hub<Color=/>"):Display();
 wait(0.5);
-v0.new("<Color=Yellow>Everyone Goes to Discord to Communicate<Color=/>"):Display();
+v0.new("<Color=Red>zzzzzz<Color=/>"):Display();
 wait(1);
 repeat
 	wait();
@@ -193,7 +193,7 @@ v5.Window = function(v134, v135)
 	v197.Position = UDim2.new(0, 30, 0.05, 0);
 	v197.Size = UDim2.new(0, 255, 0.05, 25);
 	v197.Font = Enum.Font.SourceSansBold;
-	v197.Text = "Xeter Hub";
+	v197.Text = "Phat Hub";
 	v197.TextSize = 17;
 	v197.TextColor3 = Color3.fromRGB(0, 134, 139);
 	v197.TextXAlignment = Enum.TextXAlignment.Left;
@@ -212,7 +212,7 @@ v5.Window = function(v134, v135)
 	local v223 = Instance.new("Frame");
 	v223.Name = "Tab";
 	v223.Parent = v147;
-	v223.BackgroundColor3 = Color3.fromRGB(0, 0, 0);
+	v223.BackgroundColor3 = Color3.fromRGB(25, 25, 112);
 	v223.Position = UDim2.new(0, 14, 0, 33);
 	v223.BackgroundTransparency = 1;
 	v223.Size = UDim2.new(0, 138, 0, 307);
